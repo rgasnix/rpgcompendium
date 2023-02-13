20cc884238844c1167f5957672f373146b21f9ac
Guilherme A. de Souza <rgasnix@gmail.com>
2023-02-13 18:54:10 -0300

    feat: Added barbarian class

---
+++ src/scripts/classes/barbarian.py
@@ -0,0 +1,10 @@
+class Barbarian():
+    def __init__(self, strength, dexterity, constituiton, intelligence, wisdom, charisma):
+        self.attributes = {
+            "str": strength,
+            "dex": dexterity,
+            "con": constituiton,
+            "int": intelligence,
+            "wis": wisdom,
+            "cha": charisma,
+        }
