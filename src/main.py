02ad4d680aa3d626c28e0a870433594ddedf217a
Guilherme A. de Souza <rgasnix@gmail.com>
2023-02-14 00:52:10 -0300

    gui: Started PyGTK window

--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="RPG Compendium")
+
 if __name__ == "__main__":
-    print("OK")
+    win = MyWindow()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
